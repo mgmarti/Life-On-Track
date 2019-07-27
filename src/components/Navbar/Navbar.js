037655865078765b4c497983909932f4aa58dcc4
monica <mogimartinez@gmail.com>
2019-07-27 13:47:09 -0500

    added routes

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+
+class Navbar extends Component {
+    render() {
+        return (
+            <div>
+
+                <nav class="navbar navbar-light bg-light">
+                    <a class="navbar-brand" href="#">LIFEonTRACK</a>
+                    <a href="/about">About</a>
+                </nav>
+
+            </div>
+        )
+    }
+}
+
+
+export default Navbar;
