18c42a459463e692df7bdfd13725b59093ac5a5a
moises eastland <dewaynepade95@gmail.com>
2019-07-31 20:58:41 -0500

    created and tested server.js and routes folder

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>cla clean-up done</h1>
+      </div>
+    );
+  }
+  
+}
+
+export default App;
