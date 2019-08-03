35b052096192f0cd6f86cbc0d62afdcf31c118f3
monica <mogimartinez@gmail.com>
2019-08-03 12:11:50 -0500

    added mongo

--- server.js
+++ server.js
@@ -1,40 +1,17 @@
 const express = require('express');
-// const connectDB = require('./config/db');
+const connectDB = require('./config/db');
 // const path = require('path');
 
 //Initialize express
 const app = express();
 
+
+// Connect Database
+connectDB();
+
 //Test Route
 app.get('/', (req, res) => res.send('Hello world!'));
 
-// Connect Database
-// connectDB();
-
-// Init Middleware
-// app.use(express.json({
-//     extended: false
-// }));
-
-<<<<<<< HEAD
-// Define Routes
-// app.use('/api/users', require('./routes/api/users'));
-// app.use('/api/auth', require('./routes/api/auth'));
-// app.use('/api/profile', require('./routes/api/profile'));
-// app.use('/api/posts', require('./routes/api/posts'));
-=======
-app.get("/", (req,res)=> res.render('../client/pages/About.js'));
->>>>>>> aaaa79330f094a6d849885f0c979d150228fbe91
-
-// Serve static assets in production
-// if (process.env.NODE_ENV === 'production') {
-//     // Set static folder
-//     app.use(express.static('client/build'));
-
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//     });
-// }
 
 const PORT = process.env.PORT || 3000;
 
