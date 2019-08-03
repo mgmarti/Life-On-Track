98c2b2d8b34e9eb484a33ad9637c4d5f6d93a03e
monica <mogimartinez@gmail.com>
2019-08-03 11:48:13 -0500

    new server file

--- server.js
+++ server.js
@@ -8,7 +8,29 @@ const app = express();
 //Test Route
 app.get('/', (req, res) => res.send('Hello world!'));
 
+// Connect Database
+// connectDB();
 
+// Init Middleware
+// app.use(express.json({
+//     extended: false
+// }));
+
+// Define Routes
+// app.use('/api/users', require('./routes/api/users'));
+// app.use('/api/auth', require('./routes/api/auth'));
+// app.use('/api/profile', require('./routes/api/profile'));
+// app.use('/api/posts', require('./routes/api/posts'));
+
+// Serve static assets in production
+// if (process.env.NODE_ENV === 'production') {
+//     // Set static folder
+//     app.use(express.static('client/build'));
+
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//     });
+// }
 
 const PORT = process.env.PORT || 3000;
 
