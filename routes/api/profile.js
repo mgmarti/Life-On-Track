eb809fa9121c7a574960e380f5c59e32571fd4c4
monica <mogimartinez@gmail.com>
2019-08-03 22:35:40 -0500

    can create and update user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,96 @@
 const express = require("express");
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+const {
+    check,
+    validationResult
+} = require('express-validator');
+
 
 //@route GET   api/profile
 //@desc        Test route
 //@access      Public
-router.get('/', (req, res) => res.send('Profile route'));
+// router.get('/', (req, res) => res.send('Profile route'));
+
+
+//@route GET   api/profile/me
+//@desc        Get current user's profile
+//@access      Private
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({
+            user: req.user.id
+        }).populate('user', ['name', 'avatar']);
+
+        if (!profile) {
+            return res.status(400).json({
+                msg: 'There is no profile for this user.'
+            });
+        }
+
+        res.json(profile);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+
+//@route POST  api/profile
+//@desc        create or update user profile
+//@access      Private
+
+router.post(
+    '/',
+    [
+      auth,
+      [
+        check('name', 'Name is required')
+          .not()
+          .isEmpty()
+      ]
+    ],
+    async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+  
+      const {
+        name
+      } = req.body;
+  
+    // Build profile object
+      const profileFields = {};
+      profileFields.user = req.user.id;
+      if (name) profileFields.name = name;
+
+    //   console.log(name)
+    //   res.send('Hello')
+  
+      try {
+        // Using upsert option (creates new doc if no match is found):
+        let profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true, upsert: true }
+        );
+        res.json(profile);
+
+
+        //Create
+        profile = new Profile(profileFields);
+        await profile.save();
+            res.json(profile);
+
+      } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+      }
+    }
+  );
 
 module.exports = router;
-
