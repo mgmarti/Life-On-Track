998a5228e3c400ffd78582613533c08c6cad6f1b
monica <mogimartinez@gmail.com>
2019-08-12 22:44:52 -0500

    fixed styling for goals page

--- client/src/components/CreateGoalForm/index.js
+++ client/src/components/CreateGoalForm/index.js
@@ -2,6 +2,7 @@ import React, { useRef } from "react";
 import { useStoreContext } from "../../utils/GlobalState";
 import { ADD_GOAL, LOADING } from "../../utils/actions";
 import API from "../../utils/API";
+import './GoalForm.css';
 
 function CreateGoalForm() {
   const titleRef = useRef();
@@ -30,17 +31,24 @@ function CreateGoalForm() {
   };
 
   return (
-    <div>
-      <h1>Create a Goal</h1>
-      <form className="form-group mt-5 mb-5" onSubmit={handleSubmit}>
-        <input className="form-control mb-5" required ref={titleRef} placeholder="Title" />
-        <textarea className="form-control mb-5" required ref={bodyRef} placeholder="Body" />
-        <input className="form-control mb-5" ref={authorRef} placeholder="Category" />
-        <button className="btn btn-success mt-3 mb-5" disabled={state.loading} type="submit">
-          Save Goal
-        </button>
+    <div className="goal-container">
+      <h2 className='large text-primary'>Create a Goal</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input className="form-control text-title" required ref={titleRef} placeholder="Title" />
+        </div>
+        <div className="form-group">
+          <textarea className="form-control text-space" required ref={bodyRef} placeholder="Body" />
+        </div>
+        <input className="form-control text-category" ref={authorRef} placeholder="Category" />
+        <div className="submitcontainer">
+          <button className="btn button-bg" disabled={state.loading} type="submit">
+            Save Goal
+          </button>
+        </div>
       </form>
     </div>
+
   );
 }
 
